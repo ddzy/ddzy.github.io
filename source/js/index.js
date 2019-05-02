99251f5a6526d8ef556d96e2fe2c0d055937d416
yang <1766083035@qq.com>
2019-05-02 10:10:05 +0800

    :sparkles: change pages background

--- source/js/index.js
+++ source/js/index.js
@@ -1,3 +1,13 @@
-new ColorfulBubble({
+window.onload = () => {
+  initColorfulBubble();
+}
 
-});
+function initColorfulBubble() {
+  new ColorfulBubble({
+    container: '#color-bubble-wrapper',
+    cvsWidth: window.innerWidth,
+    cvsHeight: window.innerHeight,
+    cvsBgColor: '#fff',
+    bubbleNum: 100,
+  });
+}
