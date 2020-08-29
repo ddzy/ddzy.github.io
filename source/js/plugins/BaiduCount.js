9dd0fe1757b027e8cb74e9a3c5eddc4c6eff9695
ddzy <1766083035@qq.com>
2020-08-29 09:42:48 +0800

    :rocket: update baidu count

--- source/js/plugins/BaiduCount.js
+++ source/js/plugins/BaiduCount.js
@@ -2,10 +2,22 @@
  * @description 百度统计
  */
 
+// var _hmt = _hmt || [];
+// (function() {
+//   var hm = document.createElement("script");
+//   hm.src = "https://hm.baidu.com/hm.js?15d487196b38b387297e4b5ee4a8c72d";
+//   var s = document.getElementsByTagName("script")[0];
+//   s.parentNode.insertBefore(hm, s);
+// })();
+
+/**
+ * @description 新的百度统计
+ * @summary 已将原主页由 https://ddzy.github.io(https://blog.yyge.top) 迁移到 https://yyge.top
+ */
 var _hmt = _hmt || [];
-(function() {
+(function () {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?15d487196b38b387297e4b5ee4a8c72d";
+  hm.src = "https://hm.baidu.com/hm.js?94238d821840a36e7752ab33d633bc93";
   var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
